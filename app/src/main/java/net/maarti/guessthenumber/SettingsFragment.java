36f3441b008bac3706bc4eef1a076995f8d05b4a
Maarti <bryan.martinet@gmail.com>
2016-01-24 19:28:04 +0100

    Prevent user from entering an empty username

--- app/src/main/java/net/maarti/guessthenumber/SettingsFragment.java
+++ app/src/main/java/net/maarti/guessthenumber/SettingsFragment.java
@@ -8,6 +8,7 @@ import android.os.Vibrator;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 import net.maarti.guessthenumber.utility.MultimediaManager;
 
@@ -30,7 +31,7 @@ public class SettingsFragment extends android.preference.PreferenceFragment {
 
         // On récupère la valeur du username enregistrée dans les preferences
         Context hostActivity = getActivity();
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(hostActivity);
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(hostActivity);
         String username = preferences.getString(MainMenuActivity.USERNAME_LABEL, getString(R.string.defautUsername));
 
         // On récupère le widget préférence Username
@@ -45,7 +46,13 @@ public class SettingsFragment extends android.preference.PreferenceFragment {
         prefUsername.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                prefUsername.setSummary(newValue.toString());
+                // Interdire la saisie d'un nom vide
+                if (newValue.toString().equals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.toat_must_enter_name,Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                // On met à jour le summary avec le nouveau pseudo
+                preference.setSummary(newValue.toString());
                 return true;
             }
         });
